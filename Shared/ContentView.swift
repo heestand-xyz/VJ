0c48e7bb30686f554e014498495eb50439d9c520
Anton ⬢ Hexagons <anton.heestand@gmail.com>
2020-08-12 18:50:04 +0200

    Interact

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -6,25 +6,48 @@
 //
 
 import SwiftUI
+import MultiplatformTypes
 
 struct ContentView: View {
+    
     @EnvironmentObject var vj: VJModel
+    
     var body: some View {
-        Circle()
-            .onTapGesture {
-                vj.opacity = 0.0
-                withAnimation {
-                    vj.opacity = 1.0
-                }
+        
+        ZStack {
+            
+            // Gradient Radial
+            GeometryReader { geo in
+                RadialGradient(gradient: Gradient(colors: [Color.clear, Color.primary]), center: .center, startRadius: 0.0, endRadius: geo.size.width / 2)
             }
+            
+            // Circle
+            ZStack {
+                let width: CGFloat = 10
+                Circle()
+                    .foregroundColor(vj.tap ? .primary : .clear)
+                    .padding(width)
+                Circle()
+                    .strokeBorder(lineWidth: width)
+                    .foregroundColor(vj.tap ? .clear : .primary)
+            }
+            
+            InteractView { tap in
+                vj.tap = tap
+            }
+            
+        }
             .opacity(vj.opacity)
             .padding()
     }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .frame(width: 1920 * 0.25,
+                   height: 1080 * 0.25)
             .environmentObject(VJModel())
     }
 }
--- Shared/VJModel.swift
+++ Shared/VJModel.swift
@@ -2,7 +2,9 @@
 //  VJ.swift
 //  VJ
 //
-//  Created by Cappuccino on 2020-08-12.
+//  Created by Hexagons with Cappuccino on 2020-08-12.
+//
+//  Noise is just distant reflected sounds.
 //
 
 import Foundation
@@ -10,5 +12,18 @@ import Foundation
 class VJModel: ObservableObject {
     
     @Published var opacity: Double = 1.0
+    
+    @Published var tap: Bool = false
+//    @Published var tapIndexPre: Int = 0
+//    @Published var tapIndexPost: Int = 0
+
+//    func onTap() {
+//        tap = true
+//        tapIndexPre += 1
+//        RunLoop.current.add(Timer(timeInterval: 1.0 / 6.0, repeats: false, block: { _ in
+//            self.tap = false
+//            self.tapIndexPost += 1
+//        }), forMode: .common)
+//    }
     
 }
