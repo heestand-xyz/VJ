7dafbfb189ce0cbb871dc8bb7dd9bddcfc697b5d
Anton ⬢ Hexagons <anton.heestand@gmail.com>
2020-08-12 18:13:05 +0200

    Initial Commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Cappuccino on 2020-08-12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/VJApp.swift
@@ -0,0 +1,17 @@
+//
+//  VJApp.swift
+//  Shared
+//
+//  Created by Cappuccino on 2020-08-12.
+//
+
+import SwiftUI
+
+@main
+struct VJApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
